dbf3f49c2b698b69e9ad4357a6ef6a658914a236
Tridev9 <triddev123@gmail.com>
2025-05-09 20:59:58 +0530

    Updates

---
+++ precache-manifest.cf5a066f3b375764940e27d0f860be68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253051b475e082d7ca1d0bce12fa5bc9",
+    "url": "/medical-symptom-checker/index.html"
+  },
+  {
+    "revision": "a0a7d1caf88d3c1e55ef",
+    "url": "/medical-symptom-checker/static/css/main.e4c7b706.chunk.css"
+  },
+  {
+    "revision": "72a42049fbfde9db2148",
+    "url": "/medical-symptom-checker/static/js/2.41912ad0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/medical-symptom-checker/static/js/2.41912ad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0a7d1caf88d3c1e55ef",
+    "url": "/medical-symptom-checker/static/js/main.ba5dc06b.chunk.js"
+  },
+  {
+    "revision": "b465013456e6c1586541",
+    "url": "/medical-symptom-checker/static/js/runtime-main.fe4e6ec8.js"
+  }
+]);
